00e18af1072bb5a9c4467a565401ae8031d779e4
Akshobya Rao <akshobyaraoglobal@gmail.com>
2025-08-17 16:59:23 -0500

    update
    experimenting with a fix to make the regex tester easier to use;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,43 +7,51 @@ export default function Home() {
 
   const handleTesting = () => {
     if (!text || !regex) return;
-    const reg = regex.match(/^\/(.+)\/([a-z]*)$/i);
+    const reg = regex.trim().match(/^\/(.+)\/([a-z]*)$/i);
     if (!reg) return;
 
-    const newRegex = new RegExp(reg[1], reg[2]); // first idx returns the entire string
-
-    const match = text.match(newRegex);
-    setRes(match ? match.join(" ,") : String(match));
+    if (regex.includes("/")) {
+      const newRegex = new RegExp(reg[1], reg[2]); // first idx returns the entire string
+      const match = text.match(newRegex);
+      setRes(match ? String(match) : "Please try again");
+    } else {
+      const newRegex = new RegExp(regex, "gi");
+      const match = text.match(newRegex);
+      setRes(match ? String(match) : "Please try again");
+    }
   };
 
   return (
-    <div>
-      <fieldset className="fieldset">
-        <legend className="fieldset-legend">Tester Text</legend>
-        <input
-          type="text"
-          onChange={(e) => setText(e.target.value)}
-          className="input"
-          placeholder="Type here"
-          required
-        />
-      </fieldset>
-      <fieldset className="fieldset">
-        <legend className="fieldset-legend">Regex</legend>
-        <input
-          type="text"
-          className="input"
-          onChange={(e) => setRegex(e.target.value)}
-          placeholder="Type here"
-          required
-        />
-      </fieldset>
-      <button className="btn btn-primary rounded-md" onClick={handleTesting}>
-        Match Regex
-      </button>
-      <p>
-        {res}, {text}, {String(regex)}
-      </p>
-    </div>
+    <main className="flex flex-col items-center justify-center gap-5 w-fit">
+      <div className="flex flex-col items-center justify-center gap-5 w-fit">
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Tester Text</legend>
+          <input
+            type="text"
+            onChange={(e) => setText(e.target.value)}
+            className="input"
+            placeholder="Type here"
+            required
+          />
+        </fieldset>
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Regex</legend>
+          <input
+            type="text"
+            className="input"
+            onChange={(e) => setRegex(e.target.value)}
+            placeholder="Type here"
+            required
+          />
+        </fieldset>
+        <button
+          className="btn btn-primary rounded-md w-full"
+          onClick={handleTesting}
+        >
+          Match Regex
+        </button>
+      </div>
+      <p className="font-extrabold">Result: {res}</p>
+    </main>
   );
 }
